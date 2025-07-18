2f69485e635321e18ab11a7236278403acb05290
Peterkamesa <petmaish1@gmail.com>
2025-07-18 16:32:11 +0300

    removed backend file from the root folder

--- models/report.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const reportSchema = new mongoose.Schema({
-    content: {type: String, required: true},
-    type: {type: String, required: true, enum: ['METAR', 'SYNOP', 'ACTUALS']},
-    createdAt: {type: Date, default: Date.now},
-});
-
-module.exports = mongoose.model('Report', reportSchema);
